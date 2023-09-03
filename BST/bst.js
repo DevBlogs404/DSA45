20951f53616ac37ff7a3070481d065d15df51c84
shubham <hoodashubham92@gmail.com>
2023-09-03 13:31:47 +0530

    BST class creation,node  insertion, find method implemented

---
+++ BST/bst.js
@@ -0,0 +1,76 @@
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  // inserting/ adding/ appending  nodes
+  insert(val) {
+    let newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (val === current.value) return undefined; // to check if value already exists and avoid duplication
+        if (val < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+
+  // finding nodes in a tree
+  find(val) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+
+    while (current && !found) {
+      if (val < current.value) {
+        current = current.left;
+      } else if (val > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return false;
+    return current;
+  }
+}
+
+let tree = new BinarySearchTree();
+console.log(tree);
+tree.insert(10);
+tree.insert(20);
+tree.insert(8);
+tree.insert(30);
+tree.insert(3);
+tree.insert(1);
+tree.insert(9);
+tree.insert(50);
+tree.insert(22);
+tree.insert(44);
+console.log(tree.find(10));
+console.log(tree);
